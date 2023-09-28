c81e693277657141ecaf66fb31db8b5106f3b0ea
hebelia <heberomeu@gmail.com>
2023-09-28 15:12:49 -0300

    adding structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import "./App.css";
+// import "./App.css";
 
-function App() {
-  return <>
-  
-  
-  
-  </>;
-}
+// function App() {
+//   return <>
+
+//   </>;
+// }
+
+// export default App;
+import { ConfigProvider } from "antd";
+import React from "react";
+
+const App: React.FC = () => (
+  <ConfigProvider 
+  theme={{ 
+    token: { 
+      colorPrimary: "#00b96b" 
+    }
+    }}>
+    <MyApp />
+  </ConfigProvider>
+);
 
 export default App;
