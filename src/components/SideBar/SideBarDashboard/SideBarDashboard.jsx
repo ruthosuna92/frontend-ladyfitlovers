47317dcfd750b178e051f86003f6abff36f437e8
Ruth <alejandraop391@gmail.com>
2023-10-01 18:56:42 -0500

    trabajando en la dashboard, estructura de la sidebar

--- src/components/SideBar/SideBarDashboard/SideBarDashboard.jsx
+++ src/components/SideBar/SideBarDashboard/SideBarDashboard.jsx
@@ -1,10 +1,65 @@
 
-
+import {
+  AppstoreOutlined,
+  CalendarOutlined,
+  LinkOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import { Divider, Menu, Switch } from 'antd';
+import { useState } from 'react';
 const SideBarDashboard = () => {
-
-    return <div>
-        <p>Soy la SideBar</p>
-    </div>
-}
-
-export default SideBarDashboard
+    function getItem(label, key, icon, children) {
+      return {
+        key,
+        icon,
+        children,
+        label,
+      };
+    }
+    const items = [
+      getItem('Navigation One', '1', <MailOutlined />),
+      getItem('Navigation Two', '2', <CalendarOutlined />),
+      getItem('Navigation Two', 'sub1', <AppstoreOutlined />),
+      getItem('Navigation Three', 'sub2', <SettingOutlined />),
+      getItem(
+        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+          Ant Design
+        </a>,
+        'link',
+        <LinkOutlined />,
+      ),
+    ];
+    const handleMenu = ({ item, key, keyPath, selectedKeys, domEvent }) => {
+        console.log(selectedKeys);
+    }
+//   const [mode, setMode] = useState('inline');
+//   const [theme, setTheme] = useState('light');
+//   const changeMode = (value) => {
+//     setMode(value ? 'vertical' : 'inline');
+//   };
+//   const changeTheme = (value) => {
+//     setTheme(value ? 'dark' : 'light');
+//   };
+  return (
+    <>
+      {/* <br />
+      <br /> */}
+      <Menu
+        style={{
+            width: 256,
+        }}
+        defaultSelectedKeys={['1']}
+        //defaultOpenKeys={['sub1']}
+        mode="inline"
+        theme="light"
+        items={items}
+        onSelect={handleMenu}
+      />
+        {/* <Switch onChange={changeMode} /> Change Mode
+        <Divider type="vertical" />
+        <Switch onChange={changeTheme} /> Change Style */}
+    </>
+  );
+};
+export default SideBarDashboard;
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -2,10 +2,15 @@ import SideBarDashboard from "../../components/SideBar/SideBarDashboard/SideBarD
 
 const Dashboard = () => {
 
-    return <div>
-        <p>Tablero</p>
-        <SideBarDashboard/>
+    return  <div style={{ display: 'flex', height: '100vh' }}>
+    {/* Barra lateral */}
+    <SideBarDashboard />
+    {/* Contenido principal */}
+    <div style={{ flex: 1, overflowY: 'auto' }}>
+      <p>Tablero</p>
+      {/* Agrega aquí el contenido principal de tu tablero */}
     </div>
+  </div>
 }
 
 export default Dashboard
