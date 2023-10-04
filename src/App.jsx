f8527022b6349dc6940f90c01c77fb08ceba6356
hebelia <heberomeu@gmail.com>
2023-10-04 11:46:10 -0300

    feat(LoginModal): adjusting login styles

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ const App = () => {
           fontFamily: "'Poppins', sans-serif",
           colorLink: "#C04C98",
           colorInfo: "#C04C98",
+          colorSplit: "black"
           // colorBgBase: "#E0B3D3",
           // boxShadow: ,
           // boxShadowSecondary: ,
