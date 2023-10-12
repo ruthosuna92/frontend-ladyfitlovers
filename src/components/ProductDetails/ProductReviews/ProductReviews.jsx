9ed3de47c4efe6619a6af8eb581acc7a2d4e0e2b
hebelia <heberomeu@gmail.com>
2023-10-11 22:54:07 -0300

    feat(Reviews):structure creation

--- src/components/ProductDetails/ProductReviews/ProductReviews.jsx
+++ src/components/ProductDetails/ProductReviews/ProductReviews.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+//styles
+import style from "./productReviews.module.css";
 
 //actions
-import getReviewsByProduct from
 //components
-import Review from './Review/Review'
+import Review from "./Review/Review";
+//antd
+import { Pagination } from "antd";
 
-const ProductReviews = () => {
+const ProductReviews = ({ productData }) => {
+  const reviews = productData.reviews;
+
+  const [current, setCurrent] = useState(3);
+  const onChange = (page) => {
+    console.log(page);
+    setCurrent(page);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={style.reviewsContainer}>
+      {/* mapear las reviews del producto que vienen en un array */}
 
-export default ProductReviews
+      {reviews.map(({ id, reviewText, rating, updatedAt, user }) => {
+        //id, reviewText, rating,updatedAt,user.id
+        return (
+          <>
+            <Review
+              key={id}
+              id={id}
+              reviewText={reviewText}
+              rating={rating}
+              updatedAt={updatedAt}
+              user={user}
+            />
+          </>
+        );
+      })}
+      <Pagination current={current} onChange={onChange} total={50} />
+    </div>
+  );
+};
+
+export default ProductReviews;
--- src/components/ProductDetails/ProductReviews/Review/Review.jsx
+++ src/components/ProductDetails/ProductReviews/Review/Review.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+
+//antd
+import { Avatar, Button, Col, Row, Rate, Card, Skeleton, Switch } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+const { Meta } = Card;
 
 const Review = () => {
+  const [loading, setLoading] = useState(true);
+  const onChange = (checked) => {
+    setLoading(!checked);
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div>
+        <p>Soy la review</p>
+      </div>
+      <Switch checked={!loading} onChange={onChange} />
 
-export default Review
+      <Card
+        style={{
+          width: 300,
+          marginTop: 16,
+        }}
+        loading={loading}
+      >
+        <Meta
+          avatar={
+            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />
+          }
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+      <Card
+        style={{
+          width: 300,
+          marginTop: 16,
+        }}
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <Skeleton loading={loading} avatar active>
+          <Meta
+            avatar={
+              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=2" />
+            }
+            title="Card title"
+            description="This is the description"
+          />
+        </Skeleton>
+      </Card>
+    </>
+  );
+};
+
+export default Review;
