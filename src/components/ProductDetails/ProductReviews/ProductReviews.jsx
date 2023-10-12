1c82aaa776fa86bc2663c8500b79f97363b857b6
hebelia <heberomeu@gmail.com>
2023-10-12 01:27:56 -0300

    feat(Reviews):structure

--- src/components/ProductDetails/ProductReviews/ProductReviews.jsx
+++ src/components/ProductDetails/ProductReviews/ProductReviews.jsx
@@ -4,14 +4,21 @@ import style from "./productReviews.module.css";
 
 //actions
 //components
-import Review from "./Review/Review";
+import Review from "../Review/Review";
 //antd
 import { Pagination } from "antd";
 
 const ProductReviews = ({ productData }) => {
+  console.log(productData);
+  //reviews
   const reviews = productData.reviews;
-
+  //paginacion
   const [current, setCurrent] = useState(3);
+
+
+  
+
+  //paginacion
   const onChange = (page) => {
     console.log(page);
     setCurrent(page);
@@ -19,23 +26,29 @@ const ProductReviews = ({ productData }) => {
 
   return (
     <div className={style.reviewsContainer}>
-      {/* mapear las reviews del producto que vienen en un array */}
+      {/* agregar filtro por rating y ordenamiento por fecha */}
 
-      {reviews.map(({ id, reviewText, rating, updatedAt, user }) => {
-        //id, reviewText, rating,updatedAt,user.id
-        return (
-          <>
-            <Review
-              key={id}
-              id={id}
-              reviewText={reviewText}
-              rating={rating}
-              updatedAt={updatedAt}
-              user={user}
-            />
-          </>
-        );
-      })}
+      {/* mapear las reviews del producto que vienen en un array */}
+      {!reviews ? (
+        <>
+          <h4>
+            Actualmente este producto no cuenta con reseñas, ¡sé el primero!
+          </h4>
+        </>
+      ) : (
+        reviews.map(({ id, reviewText, rating, updatedAt, user }) => (
+          //id, reviewText, rating,updatedAt,user.id
+          <Review
+            key={id}
+            id={id}
+            reviewText={reviewText}
+            rating={rating}
+            updatedAt={updatedAt}
+            user={user}
+          />
+        ))
+      )}
+      {/* cambiar paginacion para que dependa de si hay reviews / de la cantidad que haya */}
       <Pagination current={current} onChange={onChange} total={50} />
     </div>
   );
