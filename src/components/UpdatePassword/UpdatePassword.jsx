41509d5d9a425b4dc483c95a561b8f963a6d8703
altair201 <wilsondavianpa1998@outlook.com>
2023-10-11 20:01:17 -0500

    update password

--- src/components/UpdatePassword/UpdatePassword.jsx
+++ src/components/UpdatePassword/UpdatePassword.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import { Input, Divider } from "antd";
 import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import updateUser from "../../redux/Actions/User/updateUser";
 
 const UpdatePassword = () => {
-  const [error, setError] = useState("");
+  const dispatch = useDispatch()
+  const [error, setError] = useState({
+    errorPasscurrent: "",
+    errorPassNew: ""
+  });
+  console.log(error);
   const [updatePassword, setUpdatePassword] = useState({
     passwordActual: "",
     passwordNueva: "",
     passwordNueva2: "",
   });
-
+  useEffect(()=>{
+    if (updatePassword.passwordNueva === updatePassword.passwordNueva2) {
+      setError({
+        ...error,
+        passwordNueva:""
+      });
+    }
+    validate()
+  },[updatePassword.passwordNueva2, updatePassword.passwordActual])
   const handleChangePasswordActual = (event) => {
     const { value } = event.target;
     setUpdatePassword({
@@ -27,16 +43,33 @@ const UpdatePassword = () => {
   };
 
   const validate = () => {
-    if (updatePassword.passwordNueva === updatePassword.passwordNueva2) {
-      setError("");
-    } else {
-      setError("Las contraseñas nuevas no coinciden");
+    const errorpass={
+      error1:"",
+      error2:""
     }
+    if (updatePassword.passwordNueva !== updatePassword.passwordNueva2) {
+      errorpass.error1="Las contraseñas nuevas no coinciden"
+    }
+    if(updatePassword.passwordActual === ""){
+      errorpass.error2="Tienes que colocar tu contraseña actual"
+    }else{
+      errorpass.error2=""
+    }
+    setError({
+      ...error,
+      errorPasscurrent: errorpass.error2,
+      errorPassNew: errorpass.error1
+    })
   };
+  const handleSumit =()=>{
+    if(error.errorPassNew === "" && error.errorPasscurrent === ""){
+      const {passwordActual, passwordNueva}=updatePassword
+      //dispatch(updateUser({passwordActual, passwordNueva}))
+    }
+  }
 
   return (
     <div>
-      {error && <p style={{ color: "red" }}>{error}</p>}
 
       <Input
         placeholder="Contraseña Actual*"
@@ -44,11 +77,13 @@ const UpdatePassword = () => {
         onChange={handleChangePasswordActual}
         value={updatePassword.passwordActual}
       />
+      {error.errorPasscurrent && <p style={{ color: "red" }}>{error.errorPasscurrent}</p>}
       <Divider orientation="left">Ingrese la nueva contraseña</Divider>
       <Input
         placeholder="Contraseña Nueva*"
         autoComplete="off"
         name="passwordNueva"
+        type="password"
         onChange={handleChangePasswordNew}
         value={updatePassword.passwordNueva}
       />
@@ -57,12 +92,14 @@ const UpdatePassword = () => {
         placeholder="Contraseña Nueva*"
         autoComplete="off"
         name="passwordNueva2"
+        type="password"
         onChange={handleChangePasswordNew}
         value={updatePassword.passwordNueva2}
-        onBlur={validate}
-      />
+        />
       <br />
-      <ButtonPrimary title="Actualizar" />
+      {error.errorPassNew && <p style={{ color: "red" }}>{error.errorPassNew}</p>}
+      
+      <ButtonPrimary title="Actualizar" onClick={handleSumit}/>
     </div>
   );
 };
