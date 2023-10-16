7f4f2f3752d84d8b36241bf059848f1654c91a12
msolangeg <marina.solange.g@gmail.com>
2023-10-16 13:01:24 -0300

    mix(home): changes home

--- src/components/Seccion/Seccion.jsx
+++ src/components/Seccion/Seccion.jsx
@@ -1,78 +1,93 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { filtByCategory } from "../../redux/Actions/Filter/filtByCategory";
 import "./seccion.css";
 
+
 const Seccion = () => {
+const dispatch = useDispatch();
+const navigate = useNavigate();
+  
+
+const handleGoToCategory = (category) => {
+  dispatch(filtByCategory(category));
+  navigate("/products");
+};
+
+
   return (
     
       <div className="center">
         <h1>Te invitamos a conocer nuestros productos</h1>
-        <div className="gallery">
-          <div className="image image--vertical">
+        <div className="gallery"> 
+          <button className="image image--vertical" onClick={()=> handleGoToCategory("BUZOS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697237738/LadyFitLovers/8_pfxbpy.jpg"
-            />
-          </div>
-          <div className="image image--big">
+            /> 
+            {/* <p clasName="textVertical">BUZOS</p> */}
+          </button>
+          <button className="image image--big" onClick={()=> handleGoToCategory("PANTALONES")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697237739/LadyFitLovers/4_sczghs.jpg"
             />
-          </div>
-          <div className="image image--horizontal">
+          </button>
+          <button className="image image--horizontal" onClick={()=> handleGoToCategory("REMERAS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697237739/LadyFitLovers/5_tpj9oq.jpg"
             />
-          </div>
-          <div className="image">
+          </button>
+          <button className="image" onClick={()=> handleGoToCategory("BIKERS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697237740/LadyFitLovers/2_gwyvyp.jpg"
             />
-          </div>
-          <div className="image image--horizontal">
+          </button>
+          <button className="image image--horizontal" onClick={()=> handleGoToCategory("MUSCULOSAS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697237740/LadyFitLovers/7_cb5zsz.jpg"
             />
-          </div>
-          <div className="image ">
+          </button>
+          <button className="image " onClick={()=> handleGoToCategory("TOPS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697237738/LadyFitLovers/10_x7hd8e.jpg"
             />
-          </div>
-          <div className="image image--big">
+          </button>
+          <button className="image image--big" onClick={()=> handleGoToCategory("CALZAS LARGAS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697237738/LadyFitLovers/9_stqldd.jpg"
             />
-          </div>
-          <div className="image image--vertical">
+          </button>
+          <button className="image image--vertical" onClick={()=> handleGoToCategory("SHORTS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697239847/LadyFitLovers/home_4_z6xeqq.png"
             />
-          </div>
-          <div className="image ">
+          </button>
+          <button className="image " onClick={()=> handleGoToCategory("BIKINIS Y ENTERIZAS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697239612/LadyFitLovers/home_na7wgk.png"
             />
-          </div>
-          <div className="image image--vertical">
+          </button>
+          <button className="image image--vertical" onClick={()=> handleGoToCategory("CAMPERAS")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697239847/LadyFitLovers/home_3_ptaiaw.png"
             />
-          </div>
-          <div className="image image--vertical">
+          </button>
+          <button className="image image--vertical" onClick={()=> handleGoToCategory("CALZAS 3-4")}>
             <img
               className="lazy-image"
               src="https://res.cloudinary.com/dv7kzlqy6/image/upload/v1697239612/LadyFitLovers/home_2_w4sley.png"
             />
-          </div>
+          </button>
         </div>
       </div>
     
