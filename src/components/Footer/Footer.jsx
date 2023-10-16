12f9e6fe1d545fcf8195465139dab4a61eaacc69
msolangeg <marina.solange.g@gmail.com>
2023-10-16 19:26:09 -0300

    mix(footer/qadna): changes css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./footer.css";
 import { Link } from "react-router-dom";
 import { InstagramOutlined, WhatsAppOutlined } from "@ant-design/icons";
+import "./footer.css";
 
 const Footer = () => {
   return (
