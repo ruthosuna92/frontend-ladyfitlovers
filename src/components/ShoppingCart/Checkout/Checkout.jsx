493773f17408baf767beb02e0dc20aa125d9e191
Ruth <alejandraop391@gmail.com>
2023-10-18 01:25:38 -0500

    push

--- src/components/ShoppingCart/Checkout/Checkout.jsx
+++ src/components/ShoppingCart/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Space, Radio, Card, Button, Select } from 'antd';
+import { Form, Input, Space, Radio, Card, Button, Select, Divider, Row ,Col, Image} from 'antd';
 import { useEffect, useState } from 'react';
 import { provincias } from '../../CreateAcountModal/Provincias';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,6 +7,7 @@ import shippingType from '../../../redux/Actions/Checkout/shippingType';
 import shippingCost from '../../../redux/Actions/Checkout/shippingCost';
 import checkout from '../../../redux/Actions/Checkout/checkout';
 import updateUser from '../../../redux/Actions/User/updateUser';
+import { getColorName } from '../../../utils/getColorName';
 
 
 
@@ -28,17 +29,25 @@ const Checkout = () => {
     address: false,
     phone: false
   })
-  const [disabled, setDisable] = useState(true)
+  const [disabled, setDisable] = useState({
+    address: true,
+    phone: true
+  })
   const user = useSelector((state) => state.user)
   const cart = useSelector((state)=> state.cart)
   const typeSelected = useSelector((state) => state.shippingType)
   const costSelected = useSelector((state) => state.shippingCost)
+  const accessToken = useSelector((state) => state.accessToken)
   const total = cart.map((prod) => prod.price * prod.quantity).reduce((acc, cur) => acc + cur, 0)
-
+  const [userUpdated, setUserUpdated] = useState({
+    id: user.id,
+    address: null,
+    phone: null
+  })
   useEffect(()=>{
     dispatch(shippingType(shipping))
     dispatch(shippingCost(amount(shipping)))
-    if(user.address.calle){
+    if(user.address.calle && user.phone){
       let {calle, numero, dpto, entreCalles, localidad, codigoPostal, provincia} = user.address
       setItem({
         ...item,
@@ -52,8 +61,10 @@ const Checkout = () => {
         phone: user.phone
       })
     }
-  
+    
   },[shipping, user])
+  console.log(user.address.calle === "");
+  console.log(user);
   const handleRadio = (event) => {
     setShipping(event.target.value)
   }
@@ -87,15 +98,39 @@ const handleEdit = (event) => {
   }
 }
 const handleUpdate = (event) => {
+  const update = event.target.innerText
+  if(update === "Actualizar dirección" && item.calle && item.numero && item.entreCalles && item.codigoPostal && item.localidad && item.provincia){
+    setUserUpdated({
+      ...userUpdated,
+      address: {
+        calle: item.calle,
+        numero: item.numero,
+        dpto: item.dpto,
+        entreCalles: item.entreCalles,
+        codigoPostal: item.codigoPostal,
+        localidad: item.localidad,
+        provincia: item.provincia
+      }
+    })
+    dispatch(updateUser({id: userUpdated.id, address: userUpdated.address}, accessToken))
+  }
+  if( update === "Actualizar teléfono" && item.phone){
+    setUserUpdated({
+      ...userUpdated,
+      phone: item.phone
+    })
+    dispatch(updateUser({id: userUpdated.id, phone: userUpdated.phone}, accessToken))
+  }
   console.log(`updated ${event.target.innerText}`);
 }
 const handleCheckout = () => {
   dispatch(checkout(cart, costSelected))
 }
 console.log(item);
-  const {calle, numero, dpto, entreCalles, localidad, provincia, codigoPostal} = user.address
+const {calle, numero, dpto, entreCalles, localidad, provincia, codigoPostal} = user.address
   
 
+  
   return <>
 
     <Space
@@ -160,11 +195,7 @@ console.log(item);
             >
               <Form.Item
                 name="Calle"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
+                
                 style={{
                   display: 'inline-block',
                   width: 'calc(33% - 8px)',
@@ -174,11 +205,7 @@ console.log(item);
               </Form.Item>
               <Form.Item
                 name="Número"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
+               
                 style={{
                   display: 'inline-block',
                   width: 'calc(33% - 8px)',
@@ -189,11 +216,7 @@ console.log(item);
               </Form.Item>
               <Form.Item
                 name="Dpto"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
+                
                 style={{
                   display: 'inline-block',
                   width: 'calc(33% - 8px)',
@@ -209,11 +232,7 @@ console.log(item);
             >
               <Form.Item
                 name="Entre calles"
-                rules={[
-                  {
-                    required: true
-                  },
-                ]}
+                
                 style={{
                   display: 'inline-block',
                   width: 'calc(100% - 8px)',
@@ -229,11 +248,7 @@ console.log(item);
             >
               <Form.Item
                 name="Localidad"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
+               
                 style={{
                   display: 'inline-block',
                   width: 'calc(50% - 8px)',
@@ -243,11 +258,7 @@ console.log(item);
               </Form.Item>
               <Form.Item
                 name="Código postal"
-                rules={[
-                  {
-                    required: true,
-                  }
-                ]}
+               
                 style={{
                   display: 'inline-block',
                   width: 'calc(50% - 8px)',
@@ -262,11 +273,7 @@ console.log(item);
                   marginBottom: 0
                 }}
                 name="Provincia"
-                rules={[
-                  {
-                    required: true,
-                  },
-                ]}
+               
               >
                 <Select
                   style={{
@@ -283,7 +290,7 @@ console.log(item);
             </Form.Item>
 
             <Form.Item name="actualizar" label=" " colon={false} id="prueba">
-              <Button size='small' disabled={false} name='actualizar' type="primary" htmlType="submit" style={{ marginTop: 10 }} id="prueba">
+              <Button size='small' disabled={disabled.address} name='actualizar' type="primary" htmlType="submit" style={{ marginTop: 10 }} id="prueba">
                 Actualizar dirección
               </Button>
             </Form.Item>
@@ -320,7 +327,7 @@ console.log(item);
               </Form.Item>
             </Form>
     
-          <Button size='small' name='editPhone' type='primary' style={{marginTop: 10}} onClick={handleEdit}>Actualizar teléfono</Button>
+          <Button disabled={disabled.phone} size='small' name='editPhone' type='primary' style={{marginTop: 10}} onClick={handleEdit}>Actualizar teléfono</Button>
 
         </Card>}
       </Space>
@@ -330,30 +337,81 @@ console.log(item);
         margin: 40,
         alignItems: "center"
       }}>
- <Card
-          style={{
-            width: 600,
-            height: 200,
-            margin: 20
-          }}
-        >
-          <p>Aquí muestro el total de productos</p>
 
-        </Card>
-        <Card
-          title="Resumen de compra"
-          bordered={false}
+{/* card de la derecha */}
 
-          style={{
-            width: 500
-          }}
-        >
+<Card
+  title="Resumen de compra"
+  bordered={false}
+  style={{width: 655}}
+    >
+
+{cart.length > 0 && cart.map(({ name, color, image, size, quantity, price, id }, i) => {
+  return (
+    <div key={id}>
+      <Card
+        // className={styles.productShow}
+        title={name}
+        bordered={false}
+        hoverable={true}
+        style={{
+          width: 450,
+
+          margin: 15,
+        }}
+        id={id}
+        name={size}
+        // cover={
+        //   <img
+        //     alt="example"
+        //     src={image}
+        //     style={{
+        //       objectFit: "cover",
+        //       maxWidth: "50px",
+        //       maxHeight: "50px",
+        //     }}
+        //   />
+        // }
+      >
+        <Row justify="center">
+        <Divider type="vertical" style={{ color: "white" }} />
+          <Col>
+            <div><Image alt={name} src={image && image} width={35} /></div>
+          </Col>
+          <Divider type="vertical" />
+          <Col>
+            <div>Color<br />{getColorName(color)}</div>
+          </Col>
+          <Divider type="vertical" />
+          <Col>
+            <div>Cant.<br />{quantity}</div>
+          </Col>
+          <Divider type="vertical" />
+          <Col>
+            <div>Precio x unidad<br />{price}</div>
+          </Col>
+          <Divider type="vertical" />
+          <Col>
+            <div>Total<br />{price * quantity}</div>
+          </Col>
+          <Divider type="vertical" />
+        </Row>
+        
+      </Card>
+    </div>
+  );
+})}
+
+        
+
+          <Divider />
           <p>Subtotal       ${total}</p>
           <p>Envío ${costSelected}</p>
+          <Divider />
           <p>Total ${total + costSelected}</p>
 
         <Button type='primary' disabled={editing.address || editing.phone || !calle || !numero || !dpto || !entreCalles || !localidad || !provincia || !codigoPostal || !user.phone} onClick={handleCheckout}>Ir a pagar</Button>
-        </Card>
+  </Card>
       </Space>
     </Space>
    
